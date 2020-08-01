af6356da54ba82dfa4ddf247db7ad9426eb67b04
yashi12 <yashiagarwal1812@gmail.com>
2020-08-01 20:58:05 +0530

    Add(feat): Remove file if size>10Mb

--- Registration/registrationDirectives/fileSize.directive.js
+++ Registration/registrationDirectives/fileSize.directive.js
@@ -1,26 +1,38 @@
 angular
     .module('registrationForm')
-    .directive('checkFileSize', function () {
-        return {
-            link: function (scope, elem, attr, ctrl) {
-                // console.log("elem",elem);
-                // console.log("scope",scope);
-                function bindEvent(element, type, handler) {
-                    if (element.addEventListener ) {
-                        element.addEventListener(type, handler, false);
-                    } else {
-                        element.attachEvent('on' + type, handler);
-                    }
-                }
+    .directive('checkFileSize', checkFileSize)
 
-                bindEvent(elem[0], 'change', function () {
-                    console.log("this",this);
-                    if(this.files[0].size > 1000*10){
-                        alert('File size should not be more then 10 kb:' + (this.files[0].size)/(10*1000) +'KB');
-                        myfile="";
-                    }
-                    // alert('File size:' + this.files[0].size);
-                });
+function checkFileSize() {
+    let directive = {
+        require: 'ngModel',
+        link: link,
+    }
+
+    return directive;
+
+    function link(scope, elem, attr, ctrl) {
+        // console.log("elem",elem);
+        // console.log("scope",scope);
+        function bindEvent(element, type, handler) {
+            if (element.addEventListener) {
+                element.addEventListener(type, handler, false);
+            } else {
+                element.attachEvent('on' + type, handler);
             }
         }
-    });
+
+        console.log("0", elem[0]);
+        bindEvent(elem[0], 'change', function () {
+            console.log("this", this);
+            if (this.files[0].size > 1024*1024*10) {
+                console.log(ctrl);
+                ctrl= 'File size should not be more then 10 kb:' + (this.files[0].size) / (10 * 1024*1024) + 'MB';
+                // ctrl.$modelValue = ctrl.$viewValue;
+                // ctrl.$$rawModelValue = ctrl.$modelValue;
+                console.log(ctrl);
+                alert('File size should not be more then 10 kb:' + (this.files[0].size) / (10 * 1024*1024) + 'MB');
+                elem.val(null);
+            }
+        });
+    }
+}
